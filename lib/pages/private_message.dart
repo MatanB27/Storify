579a02042ac5c52f058f273297929500161563ee
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2021-04-26 15:47:44 +0300

    chat not working 100% yet

--- lib/pages/private_message.dart
+++ lib/pages/private_message.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:storify/chat_class.dart';
@@ -10,13 +12,8 @@ import 'package:storify/widgets/loading.dart';
 import 'package:uuid/uuid.dart';
 import '../widgets/loading.dart';
 import 'home.dart';
-import 'home.dart';
-import 'home.dart';
-import 'home.dart';
-
+import 'package:timeago/timeago.dart' as timeago;
 // Global variables:
-// Current room
-String currentRoom;
 
 class PrivateMessage extends StatefulWidget {
   final String privateId;
@@ -38,6 +35,8 @@ class _PrivateMessageState extends State<PrivateMessage> {
   String chatRoomId;
   // Message Id generator
   String messageId;
+  // Current room
+  String currentRoomId;
 
   header() {
     return FutureBuilder(
@@ -86,6 +85,7 @@ class _PrivateMessageState extends State<PrivateMessage> {
                 ),
                 onPressed: () {
                   //TODO : do something
+                  print(currentRoomId);
                 }),
           ],
         );
@@ -93,152 +93,136 @@ class _PrivateMessageState extends State<PrivateMessage> {
     );
   }
 
-  //if we send a message and it the first message -
-  //it will be created in firebase
-  //TODO: NOT WORKING IF WE SEND THE FIRST MESSAGE - CHECK!!
+  // If we send a message and it the first message -
+  // It will be created in firebase
   sendMessage() async {
-    timestampNow = DateTime.now();
-    //creating message map (otherId, roomId) in userRef
-    chatRoomId = Uuid().v4();
     if (messageText.text.toString() != '') {
       //text must have words
+      print(currentRoomId);
+      DocumentSnapshot docRoom = await chatRef.doc(currentRoomId).get();
       DocumentSnapshot myDocUser = await userRef.doc(currentUserId).get();
-      Map<dynamic, dynamic> myMapCheck = myDocUser.get('messages');
-
-      if (!myMapCheck.containsKey(widget.privateId) || myMapCheck.isEmpty) {
-        userRef.doc(currentUserId).set({
-          'messages': {
-            widget.privateId: chatRoomId,
-          },
-        }, SetOptions(merge: true));
-      }
-      DocumentSnapshot otherDocUser = await userRef.doc(widget.privateId).get();
-      Map<dynamic, dynamic> otherMapCheck = otherDocUser.get('messages');
-      if (!otherMapCheck.containsKey(currentUserId) || otherMapCheck.isEmpty) {
-        userRef.doc(widget.privateId).set({
-          'messages': {
-            currentUserId: chatRoomId,
-          },
-        }, SetOptions(merge: true));
-      }
-      //creating chatRooms in chatRef
-      //TODO: create chat rooms!
-      Map<dynamic, dynamic> mapRooms = myDocUser.get('messages');
-      mapRooms.forEach((key, value) {
-        if (key == widget.privateId) {
-          currentRoom = value.toString();
-        }
-      });
-      // We are creating it before so it wont give me bugs
-      chatRef.doc(currentRoom).collection('messageId').doc(messageId);
-      //TODO: not working well, check first message
-      DocumentSnapshot docRoom = await chatRef.doc(currentRoom).get();
       String senderId = myDocUser.get('id').toString();
       String senderName = myDocUser.get('displayName').toString();
 
       if (!docRoom.exists) {
-        chatRef.doc(currentRoom).collection('messageId').doc(messageId).set({
+        chatRef.doc(currentRoomId).collection('messageId').doc(messageId).set({
           'id': senderId,
           'sender': senderName,
           'message': messageText.text.toString(),
-          'timestamp': timestampNow,
+          'timeStamp': DateTime.now(),
         });
       }
     }
+    //clearing the message after sending it
+    messageText.clear();
+  }
+
+  // Getting the current room
+  createRoomInFirebase() async {
+    // Creating message map (otherId, roomId) in userRef
+    // Creating for both current user && other user
+    chatRoomId = Uuid().v4();
+
+    DocumentSnapshot myDocUser = await userRef.doc(currentUserId).get();
+    Map<dynamic, dynamic> myMapCheck = await myDocUser.get('messages');
+
+    if (!myMapCheck.containsKey(widget.privateId) || myMapCheck.isEmpty) {
+      await userRef.doc(currentUserId).set({
+        'messages': {
+          widget.privateId: chatRoomId,
+        },
+      }, SetOptions(merge: true));
+    }
+    DocumentSnapshot otherDocUser = await userRef.doc(widget.privateId).get();
+    Map<dynamic, dynamic> otherMapCheck = await otherDocUser.get('messages');
+
+    if (!otherMapCheck.containsKey(currentUserId) || otherMapCheck.isEmpty) {
+      userRef.doc(widget.privateId).set({
+        'messages': {
+          currentUserId: chatRoomId,
+        },
+      }, SetOptions(merge: true));
+    }
+    currentRoomId = chatRoomId;
+    myMapCheck.forEach((key, value) {
+      if (key == widget.privateId) {
+        currentRoomId = value;
+      }
+    });
+    print(otherMapCheck);
   }
 
   sendMessageBlock() {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              controller: messageText,
-              decoration: InputDecoration(
-                hintText: 'Write your message...',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: () {
-                    sendMessage();
-                  },
-                ),
-              ),
-            ),
+      padding: EdgeInsets.all(8.0),
+      child: TextField(
+        controller: messageText,
+        decoration: InputDecoration(
+          hintText: 'Write your message...',
+          suffixIcon: IconButton(
+            icon: Icon(Icons.send),
+            onPressed: () {
+              sendMessage();
+            },
           ),
-        ],
+        ),
       ),
     );
   }
 
-  //disposing the text editor variable when we close the page
+  // Disposing the text editor variable when we close the page
   @override
   void dispose() {
     super.dispose();
     messageText.dispose();
   }
 
-  // Streaming all the messages from the firebase
-  messageStream() {
-    return StreamBuilder(
-      stream: chatRef
-          .doc(currentRoom)
-          .collection('messageId')
-          .orderBy('TimeStamp', descending: true)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return loading();
-        }
-        final messages = snapshot.data.docs;
-        List<MessageBubbles> messageBubbles = [];
-        for (var message in messages) {
-          final messageText = message.data()['message'];
-          final messageSender = message.data()['sender'];
-          final time = message.data()['timestamp'];
-          final currentUser = message.data()['id'];
-
-          final messageBubble = MessageBubbles(
-            sender: messageSender,
-            message: messageText,
-            timestamp: time,
-            isMe: currentUserId == currentUser,
-          );
-          messageBubbles.add(messageBubble);
-        }
-        return ListView(
-          reverse: true,
-          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-          children: messageBubbles,
-        );
-      },
-    );
+  // Initstate - get the current room.
+  @override
+  void initState() {
+    super.initState();
+    createRoomInFirebase();
   }
 
-  //TODO: check UI!
+  // Streaming all the messages from the firebase
+
   @override
   Widget build(BuildContext context) {
     return Provider<AuthService>(
       create: (context) => AuthService(),
       child: Scaffold(
-        backgroundColor: Colors.grey[350],
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(55.0),
-          child: header(),
-        ),
-        body: Column(
-          children: [
-            Flexible(
-              child: ListView(
-                children: [
-                  messageStream(),
-                ],
-              ),
+          backgroundColor: Colors.grey[350],
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(55.0),
+            child: header(),
+          ),
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                MessageStream(
+                  currentUserId: currentUserId,
+                  currentRoomId: currentRoomId,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(color: Colors.grey, width: 2.0),
+                    ),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: sendMessageBlock(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            sendMessageBlock(),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
@@ -247,7 +231,7 @@ class _PrivateMessageState extends State<PrivateMessage> {
 class MessageBubbles extends StatelessWidget {
   final String sender;
   final String message;
-  final DateTime timestamp;
+  final Timestamp timestamp;
   bool isMe;
 
   MessageBubbles({this.sender, this.message, this.timestamp, this.isMe});
@@ -259,10 +243,6 @@ class MessageBubbles extends StatelessWidget {
         crossAxisAlignment:
             isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
-          Text(
-            this.sender,
-            style: TextStyle(color: Colors.black54, fontSize: 12.0),
-          ),
           Material(
             elevation: 5,
             borderRadius: isMe
@@ -286,8 +266,63 @@ class MessageBubbles extends StatelessWidget {
               ),
             ),
           ),
+          SizedBox(
+            height: 3,
+          ),
+          Text(
+            timeago.format(this.timestamp.toDate()),
+            style: TextStyle(color: Colors.black54, fontSize: 12.0),
+          ),
         ],
       ),
     );
+  }
+}
+
+// The code where we are getting the messages in the room between the two
+// users.
+class MessageStream extends StatelessWidget {
+  final String currentRoomId;
+  final String currentUserId;
+
+  MessageStream({this.currentRoomId, this.currentUserId});
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: chatRef
+          .doc(this.currentRoomId)
+          .collection('messageId')
+          .orderBy('timeStamp', descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return loading();
+        }
+        final messages = snapshot.data.docs;
+        List<MessageBubbles> messageBubbles = [];
+        for (var message in messages) {
+          final messageText = message.data()['message'];
+          final messageSender = message.data()['sender'];
+          final time = message.data()['timeStamp'];
+          final currentUser = message.data()['id'];
+
+          final messageBubble = MessageBubbles(
+            sender: messageSender,
+            message: messageText,
+            timestamp: time,
+            isMe: this.currentUserId == currentUser,
+          );
+          messageBubbles.add(messageBubble);
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
+    ;
   }
 }
